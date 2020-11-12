8fa16c934857c62b774d464818560e3adff3e2d3
olekmorawski <36660264+olekmorawski@users.noreply.github.com>
2020-11-12 12:15:27 +0100

    UPDATE 0.0.2
    Platform movement
    Ball physics
    Camera fix

---
+++ Assets/Scripts/Platform.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    public float speed = 3;
+    [SerializeField] float minX = 4.6f;
+    [SerializeField] float maxX = -4.6f;
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        var movement = Input.GetAxis("Horizontal");
+        var platformPos = transform.position;
+        platformPos += new Vector3(movement, 0, 0) * Time.deltaTime * speed;
+        platformPos.x = Mathf.Clamp(speed, minX, maxX);
+        transform.position = platformPos;
+    }
+}
